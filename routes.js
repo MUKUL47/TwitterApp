2351ea1a8afea46970b26c4ce569b86c2e933fa6
mukul47 <mukuldut1234@gmail.com>
2019-07-20 16:26:16 +0530

    added userName@

--- routes.js
+++ routes.js
@@ -2,6 +2,13 @@
  * getHashTagInfo will take 
  * @param request of hashTag and will return a promise if resolve then render the data else catch the error
  */
+module.exports.getUser = (req,res)=>{ 
+    getUserTimeLine(req.params.username)
+    .then((msg)=> res.render("user.ejs",{message : msg.data, name :req.params.username })
+    )
+    .catch((err)=> res.render("user.ejs",{message : err})        
+    ) 
+}
 module.exports.getHashTag = (req,res)=>{ 
     getHashTagInfo(req.params.hashTag)
     .then((msg)=>  res.render("hash.ejs",{message : msg.data.statuses, hash :req.params.hashTag })
@@ -17,4 +24,8 @@ module.exports.getHashTag = (req,res)=>{
 
 function getHashTagInfo(hashTag){ 
     return twitter.config.get('search/tweets', { q: `#${hashTag}`, tweet_mode : "extended" }) 
+}
+
+function getUserTimeLine(username){
+    return twitter.config.get('statuses/user_timeline',  { screen_name: `${username}`,  tweet_mode : "extended"})
 }
